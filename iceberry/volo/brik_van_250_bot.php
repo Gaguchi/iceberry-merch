6ae649b3a0cbb96dc12b2a345913fc48f0575999
Gaguchi <gaga9393@gmail.com>
2021-11-25 14:38:11 +0300

    NTZ changes

---
+++ iceberry/volo/brik_van_250_bot.php
@@ -0,0 +1,8 @@
+</span></li>
+                  <li class="list-group-item">В КОРОБКЕ: <span id="big">15 </span>НТЗ: <span id="big" style="color:red;">6</span></li>
+                  <img class="card-img-bottom" data-src="4607067937681.png" alt="Thumbnail [100%x225]"
+                    style="width: 100%; display: block; padding:20px;" src="../assets/4607067937681.png"
+                    data-holder-rendered="true">
+                </ul>
+              </div>
+            </div>
