64bebd3d1d8c786449c74cc202ff15d4c96948b2
Gaguchi <gaga9393@gmail.com>
2022-06-12 15:49:03 +0300

    added inst

---
+++ ice_cream/iceberry/inst/esk_mango_55_top.php
@@ -0,0 +1,13 @@
+<div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                  <div class="row">
+                    <div class="col-3"><img src="../../assets/site_assets/logo_ice.png" alt="" style="width: 100%;"></div>
+                    <div class="col-9" id="header-text" >"Инстинкт"<br>ЭСК_ИНСТИНКТ_БОЛТ.МАНГО_55гр.
+                </div>
+              </div></div>
+                <img class="card-img-top" data-src="inst_esk_mango_55.png" alt="Thumbnail [100%x225]"
+                  style="object-fit: contain; width: auto; height: 166px; display: block; padding:20px;" src="../../assets/icecream_images/inst_esk_mango_55.png"
+                  data-holder-rendered="true">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">PLU: <span id="big">
