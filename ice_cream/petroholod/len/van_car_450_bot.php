d4c225160327b89799928657b8632f6174e441f9
Gaguchi <gaga9393@gmail.com>
2022-11-07 14:30:41 +0400

    caramel fix

--- ice_cream/petroholod/len/van_car_450_bot.php
+++ ice_cream/petroholod/len/van_car_450_bot.php
@@ -1,7 +1,7 @@
 </span></li>
                   <li class="list-group-item">В КОРОБКЕ: <span id="big">6</li>
-                  <img class="card-img-bottom" data-src="4620032653652.png" alt="Thumbnail [100%x225]"
-                    style="width: 100%; display: block; padding:20px;" src="../../assets/barcode/4620032653652.png"
+                  <img class="card-img-bottom" data-src="4620032653638.png" alt="Thumbnail [100%x225]"
+                    style="width: 100%; display: block; padding:20px;" src="../../assets/barcode/4620032653638.png"
                     data-holder-rendered="true">
                 </ul>
               </div>
