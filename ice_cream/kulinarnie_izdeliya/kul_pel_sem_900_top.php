2bd094b9a0882afa3e98e4a492a5dfffa63fe991
Gaguchi <gaga9393@gmail.com>
2022-11-26 19:49:09 +0400

    kulinarnie resheniya

---
+++ ice_cream/kulinarnie_izdeliya/kul_pel_sem_900_top.php
@@ -0,0 +1,13 @@
+<div class="col-md-4">
+              <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                  <div class="row">
+                    <div class="col-3"><img src="../../assets/site_assets/logo_kul.png" alt="" style="width: 100%;"></div>
+                    <div class="col-9" id="header-text" >"Кулинарные решения"<br>СЕМЕЙНЫЕ_900гр.
+                </div>
+              </div></div>
+                <img class="card-img-top" data-src="kul_pel_sem_900.png" alt="Thumbnail [100%x225]"
+                  style="object-fit: contain; width: auto; height: 166px; display: block; padding:20px;" src="../../assets/icecream_images/kul_pel_sem_900.png"
+                  data-holder-rendered="true">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">PLU: <span id="big">
